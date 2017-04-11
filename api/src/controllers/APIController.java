669fc9716b00500329a21a95815b47b991a32852
Juliano Silva <juliano.silva2011@gmail.com>
2017-04-11 10:24:26 -0300

    Alterar path do controller para /

--- api/src/controllers/APIController.java
+++ api/src/controllers/APIController.java
@@ -20,7 +20,7 @@ import br.com.ideiasages.exception.NegocioException;
 import br.com.ideiasages.model.User;
 import br.com.ideiasages.util.Util;
 
-@Path("api")
+@Path("/")
 public class APIController {
 
 	// private static String endpointUrl;
