e1ccd9eef1d1b2407c6c9160548eeec38ec0e8b5
Juliano Silva <juliano.silva2011@gmail.com>
2017-04-28 15:37:54 -0300

    #17 Adicionar frontend do cadastro de idealizador

--- frontend/WebContent/assets/js/idealizador/cadastro.js
+++ frontend/WebContent/assets/js/idealizador/cadastro.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 
 		$.ajax({
 			type: "POST",
-			url: "/api/login",
+			url: "/api/accounts/idealizator/register",
 			contentType: "application/json;charset=UTF-8",
 			data: JSON.stringify(user),
 			success: function (data) {
 				if (data.success) {
 					window.location.href = "/";
 				} else {
-					alert("Erro ao logar");
+					alert("Erro ao cadatrar idealizador");
 				}
 			}
 		});
