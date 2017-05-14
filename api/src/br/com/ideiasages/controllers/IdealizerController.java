61cc6099a314f8ee81b10db576e2f8965bbfc0a9
Mathias Voelcker <mathiasvoelcker@MacBook-Pro.local>
2017-05-14 19:46:16 -0300

    acrescentar metodo editit para controller de idealizador

--- api/src/br/com/ideiasages/controllers/IdealizerController.java
+++ api/src/br/com/ideiasages/controllers/IdealizerController.java
@@ -8,10 +8,12 @@ import br.com.ideiasages.exception.ValidationException;
 import br.com.ideiasages.model.User;
 import br.com.ideiasages.util.MensagemContantes;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -21,6 +23,10 @@ public class IdealizerController {
 	private UserBO userBO = new UserBO();
 	private UserDAO userDAO = new UserDAO();
 
+	@Context
+	private HttpServletRequest request;
+	private HttpSession session;
+	
 	@POST
 	@Path("/register")
 	@Consumes("application/json; charset=UTF-8")
@@ -42,6 +48,27 @@ public class IdealizerController {
 			response.setMessage(e.getMessage());
 		}
 
+		return response;
+	}
+	
+	@PUT
+	@Path("/edit")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	public StandardResponseDTO edit(User user) throws PersistenciaException, ValidationException {
+		StandardResponseDTO response = new StandardResponseDTO();
+		session = request.getSession();
+		User loggedUser = (User) session.getAttribute("user");
+		
+		try{
+//			user = userBO.validate(loggedUser);
+			userDAO.editUser(loggedUser);
+			
+			response.setSuccess(true);
+			response.setMessage(MensagemContantes.MSG_SUC_EDICAO_USUARIO.replace("?", user.getName()));
+		} catch(Exception e){
+			response.setMessage(e.getMessage());
+		}
 		return response;
 	}
 }
