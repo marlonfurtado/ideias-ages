b14a05aa7be0ab7863091015e9e5a842453a81c9
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-05-29 19:26:21 -0300

    #70 refatorar backend

--- api/src/br/com/ideiasages/controllers/AnalystController.java
+++ api/src/br/com/ideiasages/controllers/AnalystController.java
@@ -93,20 +93,24 @@ public class AnalystController {
 	}
 	
 	@PUT
-	@Path("/inactive")
+	@Path("/status")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	public StandardResponseDTO inactive(String cpf) throws PersistenciaException {
-		StandardResponseDTO response = new StandardResponseDTO();
+	public StandardResponseDTO changeStatus(User user) throws PersistenciaException {
+		StandardResponseDTO response = new StandardResponseDTO();		
+
+		String cpf = user.getCpf();
+		boolean status = user.isActive();
 
 		try{
-			userDAO.inactiveUser(cpf);
-			
+			userDAO.changeStatus(cpf, status);
+
 			response.setSuccess(true);
 			response.setMessage(MensagemContantes.MSG_SUC_EDICAO_USUARIO.replace("?", cpf));
 		}catch(Exception e){
 			response.setMessage(e.getMessage());
 		}
+
 		return response;
 	}
 	
--- api/src/br/com/ideiasages/dao/UserDAO.java
+++ api/src/br/com/ideiasages/dao/UserDAO.java
@@ -273,14 +273,14 @@ public class UserDAO {
 		}
 	}
 
-	public boolean inactiveUser(String cpf) throws PersistenciaException {
+	public boolean changeStatus(String cpf, boolean status) throws PersistenciaException {
 		try {
 			Connection connection = ConexaoUtil.getConexao();
 			StringBuilder sql = new StringBuilder();
 			sql.append("UPDATE user SET active = ? WHERE cpf = ?");
 			
 			PreparedStatement statement = connection.prepareStatement(sql.toString());
-			statement.setInt(1, 0);
+			statement.setBoolean(1, status);
 			statement.setString(2, cpf);
 
 			return statement.execute();
@@ -289,8 +289,6 @@ public class UserDAO {
 			e.printStackTrace();
 			throw new PersistenciaException(e);
 		}
-
-	}
-
+	}	
 
 }
