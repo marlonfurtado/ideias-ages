7aa2e64dfa87650578d938a6908c33240b4eb6f2
ViniKroth <vinicius.kroth@acad.pucrs.br>
2017-06-10 11:17:58 -0300

    #98 Campo tags, não-obrigatórios.

--- api/src/br/com/ideiasages/validator/RequiredFieldsValidator.java
+++ api/src/br/com/ideiasages/validator/RequiredFieldsValidator.java
@@ -12,12 +12,14 @@ public class RequiredFieldsValidator implements Validator {
 	public boolean validar(Map<String, Object> valores) throws ValidationException {
 		StringBuilder msgErro = new StringBuilder();
 		String value = null;
-		
+
 		try {
 			for (String key : valores.keySet()) {
 
 				value = valores.get(key).toString();
-
+				if (key.equals("tags") && (value.isEmpty() || Objects.isNull(value))) {
+					continue;
+				}
 				if (Objects.isNull(value) || value.isEmpty()) {
 					key = "'" + key.concat("'");
 					msgErro.append(MensagemContantes.MSG_ERR_CAMPOS_OBRIGATORIOS);
@@ -27,7 +29,7 @@ public class RequiredFieldsValidator implements Validator {
 		} catch (Exception e) {
 			throw new ValidationException(MensagemContantes.MSG_ERR_CAMPOS_OBRIGATORIOS);
 		}
-		
+
 		if (!msgErro.toString().isEmpty()) {
 			throw new ValidationException(msgErro.toString());
 		}
