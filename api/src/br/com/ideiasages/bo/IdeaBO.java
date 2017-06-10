a206dc1771ca9876588ca153941f4494aacb45a8
ViniKroth <vinicius.kroth@acad.pucrs.br>
2017-06-10 13:15:07 -0300

    #98 Rascunho valida somente título.

--- api/src/br/com/ideiasages/bo/IdeaBO.java
+++ api/src/br/com/ideiasages/bo/IdeaBO.java
@@ -13,90 +13,98 @@ import br.com.ideiasages.validator.RequiredFieldsValidator;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.jersey.api.MessageException;
+
 public class IdeaBO {
-    private IdeaDAO ideaDAO = new IdeaDAO();
-    private UserBO userBO = new UserBO();
-    private Map<String, Object> item;
+	private IdeaDAO ideaDAO = new IdeaDAO();
+	private UserBO userBO = new UserBO();
+	private Map<String, Object> item;
 
-    public Idea validateFields(Idea idea) throws NegocioException, ValidationException, PersistenciaException {
-        try {
-            validateRequiredFields(idea);
+	public Idea validateFields(Idea idea) throws NegocioException, ValidationException, PersistenciaException {
+		try {
+			if (isDraft(idea) && !idea.getTitle().isEmpty()) {
+				return idea;
+			}
+			validateRequiredFields(idea);
 
-            return idea;
-        } catch (ValidationException e) {
-            throw new NegocioException(e);
-        }
-    }
+			return idea;
+		} catch (ValidationException e) {
+			if (isDraft(idea) && idea.getTitle().isEmpty()) {
+				throw new MessageException("Erro ao salvar rascunho, por favor preencha o título da ideia.");
+			}
+			throw new NegocioException(e);
+		}
+	}
 
-    public boolean isDraft(Idea idea) throws NegocioException {
-        if (!idea.getStatus().equals(IdeaStatus.DRAFT)) {
-            throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
-        }
+	public boolean isDraft(Idea idea) throws NegocioException {
+		if (!idea.getStatus().equals(IdeaStatus.DRAFT)) {
+			throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public boolean isOwnedByUser(Idea idea, User user) throws NegocioException {
-        if (!idea.getUser().getCpf().equals(user.getCpf())) {
-            throw new NegocioException(MensagemContantes.MSG_NOT_AUTHORIZED);
-        }
+	public boolean isOwnedByUser(Idea idea, User user) throws NegocioException {
+		if (!idea.getUser().getCpf().equals(user.getCpf())) {
+			throw new NegocioException(MensagemContantes.MSG_NOT_AUTHORIZED);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public boolean validateRequiredFields(Idea idea) throws ValidationException {
-        RequiredFieldsValidator validator = new RequiredFieldsValidator();
-        item = new HashMap<>();
-        item.put("title", idea.getTitle());
-        item.put("description", idea.getDescription());
-        item.put("tags", idea.getTags());
-        item.put("goal", idea.getGoal());
+	public boolean validateRequiredFields(Idea idea) throws ValidationException {
+		RequiredFieldsValidator validator = new RequiredFieldsValidator();
+		item = new HashMap<>();
+		item.put("title", idea.getTitle());
+		item.put("description", idea.getDescription());
+		item.put("tags", idea.getTags());
+		item.put("goal", idea.getGoal());
 
-        return validator.validar(item);
-    }
+		return validator.validar(item);
+	}
 
-    public boolean validateStatus(String status) throws NegocioException {
-        if (status == null) {
-            throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
-        }
+	public boolean validateStatus(String status) throws NegocioException {
+		if (status == null) {
+			throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public boolean validateStatusByUser(Idea idea, User user) throws NegocioException {
-        IdeaStatus status = idea.getStatus();
+	public boolean validateStatusByUser(Idea idea, User user) throws NegocioException {
+		IdeaStatus status = idea.getStatus();
 
-        if (status != null) {
-            if (status.equals(IdeaStatus.DRAFT) || status.equals(IdeaStatus.OPEN)) {
-                if (userBO.isIdealizer(user)) {
-                    return true;
-                }
-            } else {
-                if (userBO.isAnalyst(user)) {
-                    return true;
-                }
-            }
-        } else {
-            throw new NegocioException(MensagemContantes.MSG_NOT_AUTHORIZED);
-        }
+		if (status != null) {
+			if (status.equals(IdeaStatus.DRAFT) || status.equals(IdeaStatus.OPEN)) {
+				if (userBO.isIdealizer(user)) {
+					return true;
+				}
+			} else {
+				if (userBO.isAnalyst(user)) {
+					return true;
+				}
+			}
+		} else {
+			throw new NegocioException(MensagemContantes.MSG_NOT_AUTHORIZED);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public boolean isPossibleChangeStatus(Idea idea, String newStatus) throws NegocioException {
-        String ideaStatus = idea.getStatus().name();
-        newStatus = newStatus.toUpperCase();
+	public boolean isPossibleChangeStatus(Idea idea, String newStatus) throws NegocioException {
+		String ideaStatus = idea.getStatus().name();
+		newStatus = newStatus.toUpperCase();
 
-        if (ideaStatus.equals(IdeaStatus.DRAFT.name()) && newStatus.equals(IdeaStatus.OPEN.name())) {
-            return true;
-        } else if (ideaStatus.equals(IdeaStatus.OPEN.name()) && newStatus.equals(IdeaStatus.IN_ANALYSIS.name())) {
-            return true;
-        } else if (ideaStatus.equals(IdeaStatus.IN_ANALYSIS.name()) && newStatus.equals(IdeaStatus.APPROVED.name())) {
-            return true;
-        } else if (ideaStatus.equals(IdeaStatus.IN_ANALYSIS.name()) && newStatus.equals(IdeaStatus.REJECTED.name())) {
-            return true;
-        } else {
-            throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
-        }
-    }
+		if (ideaStatus.equals(IdeaStatus.DRAFT.name()) && newStatus.equals(IdeaStatus.OPEN.name())) {
+			return true;
+		} else if (ideaStatus.equals(IdeaStatus.OPEN.name()) && newStatus.equals(IdeaStatus.IN_ANALYSIS.name())) {
+			return true;
+		} else if (ideaStatus.equals(IdeaStatus.IN_ANALYSIS.name()) && newStatus.equals(IdeaStatus.APPROVED.name())) {
+			return true;
+		} else if (ideaStatus.equals(IdeaStatus.IN_ANALYSIS.name()) && newStatus.equals(IdeaStatus.REJECTED.name())) {
+			return true;
+		} else {
+			throw new NegocioException(MensagemContantes.MSG_IDEA_IS_NOT_DRAFT);
+		}
+	}
 }
