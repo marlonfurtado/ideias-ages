6eb72327dfa162eb3a486e1c63237aad557374b3
Juliano Silva <juliano.silva2011@gmail.com>
2017-06-16 20:17:15 -0300

    Corrigir cadastro de ideia (cpf do analista deve ser nulo inicialmente)

--- api/src/br/com/ideiasages/dao/IdeaDAO.java
+++ api/src/br/com/ideiasages/dao/IdeaDAO.java
@@ -18,9 +18,9 @@ import br.com.ideiasages.util.ConexaoUtil;
  *
  **/
 public class IdeaDAO {
-    private final String ADD_COMMENT =
-        " INSERT INTO idea_has_idea_comments " +
-        " VALUES (?, ?) ";
+	private final String ADD_COMMENT =
+			" INSERT INTO idea_has_idea_comments " +
+					" VALUES (?, ?) ";
 
 	/**
 	 * Faz a consulta de uma idéia através do seu ID.
@@ -30,8 +30,8 @@ public class IdeaDAO {
 	 * @throws br.com.ideiasages.exception.PersistenciaException Exceção de operações realizadas
 	 * na base de dados.
 	 **/
-    public Idea getIdea(int id) throws PersistenciaException {
-        Idea    idea = new Idea();
+	public Idea getIdea(int id) throws PersistenciaException {
+		Idea    idea = new Idea();
 
 		try {
 			Connection connection = ConexaoUtil.getConexao();
@@ -41,28 +41,28 @@ public class IdeaDAO {
 			PreparedStatement statement = connection.prepareStatement(sql.toString());
 			statement.setInt(1, id);
 
-            ResultSet resultset = statement.executeQuery();
-            if (resultset.next()) {
-                idea.setDescription(resultset.getString("description"));
-                idea.setGoal(resultset.getString("goal"));
-                idea.setId(resultset.getInt("id"));
-                idea.setStatus(IdeaStatus.valueOf(resultset.getString("status_name")));
-                idea.setTags(resultset.getString("tags"));
-                idea.setTitle(resultset.getString("title"));
-                idea.setUser(new User(resultset.getString("user_cpf")));
-                idea.setCreationDate(resultset.getDate("creationDate"));
-            } else {
-                idea = null;
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            throw new PersistenciaException(e);
-        }
+			ResultSet resultset = statement.executeQuery();
+			if (resultset.next()) {
+				idea.setDescription(resultset.getString("description"));
+				idea.setGoal(resultset.getString("goal"));
+				idea.setId(resultset.getInt("id"));
+				idea.setStatus(IdeaStatus.valueOf(resultset.getString("status_name")));
+				idea.setTags(resultset.getString("tags"));
+				idea.setTitle(resultset.getString("title"));
+				idea.setUser(new User(resultset.getString("user_cpf")));
+				idea.setCreationDate(resultset.getDate("creationDate"));
+			} else {
+				idea = null;
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new PersistenciaException(e);
+		}
 
 		return idea;
 	}
 
-    /**
+	/**
 	 * Adiciona um comentário à idéia informada por parâmetro na base de dados.
 	 *
 	 * @param idea Objeto idéia.{@link br.com.ideiasages.model.Idea}
@@ -72,60 +72,60 @@ public class IdeaDAO {
 	 * na base de dados.
 	 *
 	 **/
-    public boolean addComment(Idea idea, IdeaComment comment) throws PersistenciaException {
-        try {
-            Connection connection = ConexaoUtil.getConexao();
-            PreparedStatement statement = connection.prepareStatement(ADD_COMMENT);
+	public boolean addComment(Idea idea, IdeaComment comment) throws PersistenciaException {
+		try {
+			Connection connection = ConexaoUtil.getConexao();
+			PreparedStatement statement = connection.prepareStatement(ADD_COMMENT);
 
-            statement.setInt(1, idea.getId());
-            statement.setLong(2, comment.getId());
+			statement.setInt(1, idea.getId());
+			statement.setLong(2, comment.getId());
 
-            return statement.execute();
+			return statement.execute();
 
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            throw new PersistenciaException(e);
-        }
-    }
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new PersistenciaException(e);
+		}
+	}
 
-    /**
- 	 * Adiciona uma idéia à na base de dados.
- 	 *
- 	 * @param newIdea Objeto idéia.{@link br.com.ideiasages.model.Idea}
- 	 * @return Verdadeiro em caso de sucesso na inclusão na base de dados.
- 	 * @throws br.com.ideiasages.exception.PersistenciaException Exceção de operações realizadas
- 	 * na base de dados.
- 	 *
- 	 **/
-    public int addIdeia(Idea newIdea) throws PersistenciaException {
-        try {
-            Connection connection = ConexaoUtil.getConexao();
-            StringBuilder sql = new StringBuilder();
-            sql.append("INSERT INTO idea(title, description, status_name, tags, user_cpf, goal, creationDate)");
-            sql.append(" VALUES(?, ?, ?, ?, ?, ?, NOW())");
-            PreparedStatement statement = connection.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, newIdea.getTitle());
-            statement.setString(2, newIdea.getDescription());
-            statement.setString(3, newIdea.getStatus().name());
-            statement.setString(4, newIdea.getTags());
-            statement.setString(5, newIdea.getUser().getCpf());
-            statement.setString(6, newIdea.getGoal());
+	/**
+	 * Adiciona uma idéia à na base de dados.
+	 *
+	 * @param newIdea Objeto idéia.{@link br.com.ideiasages.model.Idea}
+	 * @return Verdadeiro em caso de sucesso na inclusão na base de dados.
+	 * @throws br.com.ideiasages.exception.PersistenciaException Exceção de operações realizadas
+	 * na base de dados.
+	 *
+	 **/
+	public int addIdeia(Idea newIdea) throws PersistenciaException {
+		try {
+			Connection connection = ConexaoUtil.getConexao();
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO idea(title, description, status_name, tags, user_cpf, goal, creationDate, analyst_cpf)");
+			sql.append(" VALUES(?, ?, ?, ?, ?, ?, NOW(), NULL)");
+			PreparedStatement statement = connection.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, newIdea.getTitle());
+			statement.setString(2, newIdea.getDescription());
+			statement.setString(3, newIdea.getStatus().name());
+			statement.setString(4, newIdea.getTags());
+			statement.setString(5, newIdea.getUser().getCpf());
+			statement.setString(6, newIdea.getGoal());
 
-            statement.executeUpdate();
+			statement.executeUpdate();
 
-            ResultSet generatedKeys = statement.getGeneratedKeys();
+			ResultSet generatedKeys = statement.getGeneratedKeys();
 
-            if (generatedKeys.next()) {
-                return generatedKeys.getInt(1);
-            }
+			if (generatedKeys.next()) {
+				return generatedKeys.getInt(1);
+			}
 
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            throw new PersistenciaException(e);
-        }
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new PersistenciaException(e);
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 
 	/**
 	 * Altera uma idéia existente na base de dados.
@@ -136,19 +136,19 @@ public class IdeaDAO {
 	 * na base de dados.
 	 *
 	 **/
-    public boolean updateIdea(Idea newIdea) throws PersistenciaException {
-        try {
-            Connection connection = ConexaoUtil.getConexao();
-            StringBuilder sql = new StringBuilder();
-            sql.append("UPDATE idea SET title = ?, description = ?, tags = ?, goal = ?, status_name = ? WHERE id = ?");
+	public boolean updateIdea(Idea newIdea) throws PersistenciaException {
+		try {
+			Connection connection = ConexaoUtil.getConexao();
+			StringBuilder sql = new StringBuilder();
+			sql.append("UPDATE idea SET title = ?, description = ?, tags = ?, goal = ?, status_name = ? WHERE id = ?");
 
-            PreparedStatement statement = connection.prepareStatement(sql.toString());
-            statement.setString(1, newIdea.getTitle());
-            statement.setString(2, newIdea.getDescription());
-            statement.setString(3, newIdea.getTags());
-            statement.setString(4, newIdea.getGoal());
-            statement.setString(5, newIdea.getStatus().name());
-            statement.setInt(6, newIdea.getId());
+			PreparedStatement statement = connection.prepareStatement(sql.toString());
+			statement.setString(1, newIdea.getTitle());
+			statement.setString(2, newIdea.getDescription());
+			statement.setString(3, newIdea.getTags());
+			statement.setString(4, newIdea.getGoal());
+			statement.setString(5, newIdea.getStatus().name());
+			statement.setInt(6, newIdea.getId());
 
 
 			return statement.execute();
@@ -168,11 +168,11 @@ public class IdeaDAO {
 	 * na base de dados.
 	 *
 	 **/
-    public boolean updateStatus(Idea newIdea) throws PersistenciaException {
-        try {
-            Connection connection = ConexaoUtil.getConexao();
-            StringBuilder sql = new StringBuilder();
-            sql.append("UPDATE idea SET status_name = ? WHERE id = ?");
+	public boolean updateStatus(Idea newIdea) throws PersistenciaException {
+		try {
+			Connection connection = ConexaoUtil.getConexao();
+			StringBuilder sql = new StringBuilder();
+			sql.append("UPDATE idea SET status_name = ? WHERE id = ?");
 
 			PreparedStatement statement = connection.prepareStatement(sql.toString());
 			statement.setString(1, newIdea.getStatus().name());
