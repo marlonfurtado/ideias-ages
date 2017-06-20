f1cd4a4ee7d972603cd71a1509c2211df9e22259
Juliano Silva <juliano.silva2011@gmail.com>
2017-06-20 19:40:29 -0300

    #106 Adicionando modais que faltaram

--- frontend/WebContent/assets/js/system.js
+++ frontend/WebContent/assets/js/system.js
@@ -13,41 +13,31 @@ $(function() {
 
 	$.get("./api/auth/me", function (data) {
 		if (data.cpf === null) {
-			alert("Sua sessão expirou, faça login novamente.");
+			modal.show("Sua sessão expirou", "Faça login novamente.");
+
 			Cookies.remove("userName");
 			Cookies.remove("userRole");
 			Cookies.remove("userCpf");
 
-			document.location = document.location;
+			$('#myModal').on('hide.bs.modal', function () {
+				window.location.href = "./";
+			})
 		}
 	});
- /*		
-	function loadVersion() {
-			$.get("./api/version", function(data) {
-			$('#version').html(data.responseText); 
-			console.log(data);
-			alert( "AQUI NÃO ENTRA" );
-		}).always (function(data) {
-			$('#version').html(data.responseText); 
-			console.log(data)
-		});
-	}
-	
-	
-*/	
+
 	function loadVersion() {
 		$.ajax({
 			type: "GET",
 			url: "./api/version",
 			contentType: "application/text;charset=UTF-8",
 			success: function (data) {
-				console.log(data)
+				console.log(data);
 				$('#version').html(data);
 				console.log("PASSOU")
    		},
 			error: function (data) {
-   				console.log(data)
-				alert("Erro não Funfo.");
+   				console.log(data);
+				modal.show("Erro", "Falha ao requisitar informações da API.");
             }
 		});
 	}
