8745e5b1d4cdc9ffd429e8b0041bbe62f3a0ae84
larissafiorini <larissa.fiorini@hotmail.com>
2018-11-13 16:58:12 -0200

    More unit tests

---
+++ api/tests/br/com/ideasages/tests/models/IdeaCommentTests.java
@@ -0,0 +1,46 @@
+package br.com.ideasages.tests.models;
+
+import static org.junit.Assert.*;
+import br.com.ideiasages.model.*;
+
+import org.junit.Test;
+
+public class IdeaCommentTests {
+
+	@Test
+	public void testIdeaComment() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+	@Test
+	public void testGetId() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+	@Test
+	public void testSetId() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+	@Test
+	public void testGetComment() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+	@Test
+	public void testSetComment() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+	@Test
+	public void testToString() {
+		IdeaComment ic = new IdeaComment();
+		assertNotNull(ic);
+	}
+
+}
