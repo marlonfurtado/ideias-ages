0a2df31af8d65043cd12a8bdf5aa4f22eb89e1a0
larissafiorini <larissa.fiorini@hotmail.com>
2018-11-13 16:44:08 -0200

    Unit tests Models

---
+++ api/tests/br/com/ideasages/tests/models/IdeaModelTests.java
@@ -0,0 +1,136 @@
+package br.com.ideasages.tests.models;
+
+import static org.junit.Assert.*;
+import br.com.ideiasages.model.*;
+
+import org.junit.Test;
+
+public class IdeaModelTests {
+
+	@Test
+	public void testIdea() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testIdeaStringStringStringStringDate() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testIdeaIntStringStringIdeaStatusStringUserStringDate() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetCreationDate() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetCreationDate() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetId() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetId() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetTitle() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetTitle() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetDescription() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetDescription() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetStatus() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetStatus() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetTags() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetTags() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetUser() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetUser() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetGoal() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetGoal() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testGetAnalyst() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+	@Test
+	public void testSetAnalyst() {
+		Idea i = new Idea();
+		assertNotNull(i);
+	}
+
+}
---
+++ api/tests/br/com/ideiasages/tests/controllers/IdeaControllerTests.java
@@ -0,0 +1,53 @@
+package br.com.ideiasages.tests.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.ideiasages.controllers.IdeaController;
+
+public class IdeaControllerTests {
+
+	@Test
+	public void testIdeaController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLogger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLogger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+		IdeaController idc = new IdeaController();
+		Response = idc.create(body);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdea() {
+		fail("Not yet implemented");
+	}
+
+}
