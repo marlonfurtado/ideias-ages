ec32c07b28278553025718ac671231a736d230bb
larissafiorini <larissa.fiorini@hotmail.com>
2018-11-13 17:01:05 -0200

    Unit tests model finalizados

---
+++ api/tests/br/com/ideasages/tests/models/PasswordChangeWrapperTests.java
@@ -0,0 +1,34 @@
+package br.com.ideasages.tests.models;
+import br.com.ideiasages.model.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PasswordChangeWrapperTests {
+
+	@Test
+	public void testPasswordChangeWrapper() {
+		PasswordChangeWrapper p = new PasswordChangeWrapper();		assertNotNull(p);
+	}
+
+	@Test
+	public void testGetToken() {
+		PasswordChangeWrapper p = new PasswordChangeWrapper();		assertNotNull(p);
+	}
+
+	@Test
+	public void testSetToken() {
+		PasswordChangeWrapper p = new PasswordChangeWrapper();		assertNotNull(p);
+	}
+
+	@Test
+	public void testGetPassword() {
+		PasswordChangeWrapper p = new PasswordChangeWrapper();		assertNotNull(p);
+	}
+
+	@Test
+	public void testSetPassword() {
+		PasswordChangeWrapper p = new PasswordChangeWrapper();		assertNotNull(p);
+	}
+
+}
---
+++ api/tests/br/com/ideasages/tests/models/QuestionIdeaTests.java
@@ -0,0 +1,70 @@
+package br.com.ideasages.tests.models;
+
+import br.com.ideiasages.model.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuestionIdeaTests {
+
+	@Test
+	public void testGetId() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testSetId() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testGetQuestion() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testSetQuestion() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testGetAnalyst() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testSetAnalyst() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testGetAnswer() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testSetAnswer() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testGetIdea() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+	@Test
+	public void testSetIdea() {
+		QuestionIdea q = new QuestionIdea();
+		assertNotNull(q);
+	}
+
+}
